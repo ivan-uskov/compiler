b05c7d90192c1a65717a4bf90de2863c4c01f8c6
Ivan Uskov <ivan.uskov@cpslabs.net>
2018-06-14 18:36:22 +0300

    Добавил еще кейс

--- compiler.cpp
+++ compiler.cpp
@@ -18,8 +18,21 @@ int main()
 
         Tokenizer tokenizer(in, cerr);
         auto tokens = tokenizer.getTokens();
-
         Parser(Rules::get(), cout).parse(tokens);
+
+        auto r1 = Rules::Table{
+            {Token::Root, {Token::S}},
+            {Token::S, {Token::A, Token::Else, Token::B, Token::Semicolon}},
+            {Token::A, {Token::i, Token::Semicolon}},
+            {Token::A, {Token::E}},
+            {Token::B, {Token::E}},
+            {Token::E, {Token::E, Token::Plus, Token::i}},
+            {Token::E, {Token::i}}
+        };
+        auto t1 = std::deque<Token::Token>{
+            {Token::i}, {Token::Plus}, {Token::i}, {Token::Else}, {Token::i}, {Token::Semicolon}, {Token::End}
+        };
+        Parser(r1, cout).parse(t1);
     }
     catch (exception const& e)
     {
